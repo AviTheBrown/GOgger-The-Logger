e5fd3a3faca6e6909516e0dc2052d585d63adfd6
Avi Brown <avinbrown.focus@icloud.com>
2023-02-18 18:52:10 -0500

    Added helper methods for Logger struct.

--- logger.go
+++ logger.go
@@ -1,7 +1,21 @@
 package GOgger
 
-type Logger struct{
+type Logger struct {
+}
 
+// (helper to Logger struct ) (the format of the string ex. %s, %d, Variadic arg)
+func (l *Logger) Debugf(format string, args ...any) {
+	// impl
+}
+
+// (helper to Logger struct ) (the format of the string ex. %s, %d, Variadic arg)
+func (l *Logger) Infof(format string, args ...any) {
+	// impl
+}
+
+// (helper to Logger struct ) (the format of the string ex. %s, %d, Variadic arg)
+func (l *Logger) Errorf(format string, args ...any){
+    // impl
 }
 
 func main() {
