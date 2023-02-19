eac410bc603ad0f14cee4b00691d2e7cd5d71514
Avi Brown <avinbrown.focus@icloud.com>
2023-02-18 19:25:50 -0500

    added a New helper method for the Logger struct.

--- level.go
+++ level.go
@@ -4,25 +4,25 @@ package GOgger
 type Level byte
 
 const (
-    // LevelDebug -> represents the lowest level of the log,
-    // mostly used for debugging purposes.
-    LevelDebug Level = iota
+	// LevelDebug -> represents the lowest level of the log,
+	// mostly used for debugging purposes.
+	LevelDebug Level = iota
 
-    // LevelInfo -> represents a logging level that contains
-    // information deem valuable.
-    LevelInfo
+	// LevelInfo -> represents a logging level that contains
+	// information deem valuable.
+	LevelInfo
 
-    // LevelWarn -> represents a log between that of basic infomation
-    // and that of of and error.
-    LevelWarn
+	// LevelWarn -> represents a log between that of basic infomation
+	// and that of of and error.
+	LevelWarn
 
-    // LevelError -> represents the penultimate highest logging level,
-    // only used to trace errors.
-    LevelError
+	// LevelError -> represents the penultimate highest logging level,
+	// only used to trace errors.
+	LevelError
 
-    // LevelFatal -> represents a log that will cause signifigant
-    // bugs to the program
-    LevelFatal
+	// LevelFatal -> represents a log that will cause signifigant
+	// bugs to the program
+	LevelFatal
 )
 
 func main() {
