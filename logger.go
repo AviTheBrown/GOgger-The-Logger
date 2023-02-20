57b178d583b4890caef5273f6fa62fb50317c830
Avi Brown <avinbrown.focus@icloud.com>
2023-02-19 23:19:03 -0500

    Refactored everything, starting over.

--- logger.go
+++ logger.go
@@ -1,58 +1,47 @@
 package GOgger
 
-import "fmt"
 
+// Logger struct will hold information needed
+    // to log information
 type Logger struct {
-	threshold Level
+    // the Logger must have a strict imprtance level attributed
+        // to its message. User subjectivity but there is a general
+        // ideal of what should be of great and lesser importance.
+
+    // should start at Debug/trace and increase in criticalness up to Fatal.
+// --------------------------------------------------------------
+
+    // threshold -> represents the level of the logger that the user wishes
+        // to access.
+        // no need to expose this field. The internl struct of the logger of the library callers.
+    threshold Level
+}
+// Debugf method-> takes no less that one argument
+    // the Logger struct instance now can be used by
+    // the Debugf method and manipulate its fields,
+    // as its the first argument passed to the Debugf method
+func(l *Logger) Debugf(format string, args ...any) {
+
 }
 
-// Debugf -> the format of the string ex. %s, %d, Variadic arg.
-func (l *Logger) Debugf(format string, args ...any) {
-	if l.threshold > LevelDebug {
-		return
-	}
-	_, _ = fmt.Printf(format+"\n", args...)
+func(l *Logger) Infof(format string, args ...any) {
+
 }
 
-// Infof -> the format of the string ex. %s, %d, Variadic arg.
-func (l *Logger) Infof(format string, args ...any) {
-	if l.threshold > LevelInfo {
-		return
-	}
-	_, _ = fmt.Printf(format+"\n", args...)
-}
 
-// Warnf -> the format of the string ex. %s, %d, Variadic arg.
-func (l *Logger) Warnf(format string, args ...any) {
-	if l.threshold > LevelWarn {
-		return
-	}
-	_, _ = fmt.Printf(format+"\n", args...)
-}
-
-// Errorf -> the format of the string ex. %s, %d, Variadic arg.
-func (l *Logger) Errorf(format string, args ...any) {
-	if l.threshold > LevelError {
-		return
-	}
-	_, _ = fmt.Printf(format+"\n", args...)
-}
-
-// Fatalf -> the format of the string ex. %s, %d, Variadic arg.
-func (l *Logger) Fatalf(format string, args ...any) {
-	if l.threshold > LevelFatal {
-		return
-	}
-	_, _ = fmt.Printf(format+"\n", args...)
-}
-
-// New -> returns a logger, ready to log at the required threshold.
+// New -> takes one argument with of the type Level
+    // and returns a pointer to a new Logger instance
+    // with the threshold field filled with the threshold argument
+    // this will conrrespond to the Level type.
+    // *** this function invocation should be the first step in creating a Logger instance ***
 func New(threshold Level) *Logger {
-	return &Logger{
-		threshold: threshold,
-	}
+    // creates a new instance of the Logger struct
+    // and initializes its fields using a struct literal.
+    return &Logger{
+        threshold: threshold,
+    }
 }
 
-func main() {
+func main(){
 
 }
