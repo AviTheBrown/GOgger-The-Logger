109cfdf567b188f031760d43206f5a3532a1adbf
Avi Brown <avinbrown.focus@icloud.com>
2023-02-21 20:32:42 -0500

    Corrected the test cases.

--- logger_test.go
+++ logger_test.go
@@ -2,24 +2,70 @@ package GOgger_test
 
 import (
     GOgger "Gogger"
-    "os"
+    "testing"
 )
 
-func ExampleLogger_Debugf() {
-    debugLogger := GOgger.New(
-        GOgger.LevelFatal,
-        GOgger.WithOutput(os.Stdout))
-    debugLogger.Debugf("Hello %s", "world")
-    // Output:
-    // Hello world
-}
-
-// testWriter ->is a struc that implements io.Writer
 type testWriter struct {
-        contents string
+    contents string }
+// Write implements the io.Writer interface.
+func (tw *testWriter) Write(p []byte) (n int, err error) {
+tw.contents = tw.contents + string(p)
+return len(p), nil }
+
+const (
+    debugMessage = "Why write I still all one, ever the same,"
+    infoMessage = "And keep invention in a noted weed,"
+    warnMessage = "Then take the chance by the thorns"
+    errorMessage = "That every word doth almost tell my name,"
+    fatalMessage = "Why thou"
+)
+
+
+func TestLogger_DebugfInforfErrorf(t *testing.T) {
+    type testCase struct {
+        threshold GOgger.Level
+        expectedString string
     }
 
-    func (tw *testWriter) Write(p []byte) (n int, err error) {
-        tw.contents = tw.contents + string(p)
-        return len(p), nil
+    tt := map[string]testCase{
+        "debug": {
+            threshold: GOgger.LevelDebug,
+            expectedString: debugMessage +"\n" + infoMessage + "\n" + warnMessage + "\n" + errorMessage + "\n" + fatalMessage + "\n",
+        },
+        "info" : {
+            threshold: GOgger.LevelInfo,
+            expectedString: infoMessage + "\n" + warnMessage + "\n" + errorMessage + "\n" + fatalMessage + "\n",
+        },
+        "warn" : {
+            threshold: GOgger.LevelWarn,
+            expectedString: warnMessage + "\n" + errorMessage + "\n" + fatalMessage + "\n",
+        },
+        "error" : {
+            threshold: GOgger.LevelError,
+            expectedString: errorMessage + "\n" + fatalMessage + "\n",
+        },
+        "fatal" : {
+            threshold: GOgger.LevelFatal,
+            expectedString: fatalMessage +"\n",
+        },
     }
+
+    for name ,tc := range tt {
+        t.Run(name, func(t *testing.T) {
+            tw := &testWriter{}
+
+            testedLogger := GOgger.New(tc.threshold, GOgger.WithOutput(tw))
+
+            testedLogger.Debugf(debugMessage)
+            testedLogger.Infof(infoMessage)
+            testedLogger.Warnf(warnMessage)
+            testedLogger.Errorf(errorMessage)
+            testedLogger.Fatalf(fatalMessage)
+
+
+            if tw.contents != tc.expectedString {
+                t.Errorf("invlaid contents, expected: %q, got: %q", tc.expectedString, tw.contents)
+            }
+        })
+    }
+}
